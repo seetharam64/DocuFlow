a125e88ff419848d45b01b107924e7a20a016129
Seetha Ram Rao Vanam <seetharam2602@gmail.com>
2025-03-17 00:48:32 -0500

    Initial commit

---
+++ src/main/java/com/docuflow/docuflow/DocuFlowApplication.java
@@ -0,0 +1,13 @@
+package com.docuflow.docuflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocuFlowApplication.class, args);
+	}
+
+}
